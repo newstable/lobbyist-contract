5aaadec60b200a1903ba1d86b12a72b16b5dd4a8
Sasuke Uchiha <97214534+Sasuke031026@users.noreply.github.com>
2022-08-30 15:10:17 +1000

    update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,6 @@
 async function main() {
     const Token = await ethers.getContractFactory("Pool");
-    const token = await Token.deploy();
+    const token = await Token.deploy("0xbC7a300977383B531383e50c76A2bD6dB3Aa9051", 100000);
 
     console.log("Token address:", token.address);
 }
